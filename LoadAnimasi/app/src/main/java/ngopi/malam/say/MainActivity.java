1c1216afe52618662535275bc28c49d982d36f06
maskaywalanay <mwalanay@gmail.com>
2019-06-19 22:59:19 +0700

    komit pertama dari memuat animasi

--- LoadAnimasi/app/src/main/java/ngopi/malam/say/MainActivity.java
+++ LoadAnimasi/app/src/main/java/ngopi/malam/say/MainActivity.java
@@ -2,13 +2,45 @@ package ngopi.malam.say;
 
 import android.app.*;
 import android.os.*;
+import android.widget.*;
+import android.view.animation.*;
+import android.view.*;
 
 public class MainActivity extends Activity 
 {
+	// konstruktor
+	Button btn;
+	TextView txt;
+	Animation aniz, anu;
+	
+	
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+		
+		// Menemukan Objek layout 
         setContentView(R.layout.main);
+		
+		// Memuat animasi dari folder anim
+		aniz = AnimationUtils.loadAnimation(this,R.anim.zoom_enter);
+		aniz.setDuration(700);
+		anu = AnimationUtils.loadAnimation(this,R.anim.zoom_exit);
+		anu.setDuration(900);
+		
+		// Menemukan Objek Dengan ID 
+		btn = (Button)findViewById(R.id.onclick);
+		txt = (TextView)findViewById(R.id.targetnya);
+		
+		btn.setOnClickListener(new View.OnClickListener()
+			{
+				@Override
+				public void onClick(View p1)
+				{
+					// text yang akan di beri animasi
+					txt.startAnimation(aniz);
+					
+				}
+		});
     }
 }
