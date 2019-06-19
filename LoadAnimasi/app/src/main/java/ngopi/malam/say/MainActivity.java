ba1a874794e571a780fe9ce36f5c005dfe0a1810
maskaywalanay <mwalanay@gmail.com>
2019-06-19 22:38:44 +0700

    memuat animasi

---
+++ LoadAnimasi/app/src/main/java/ngopi/malam/say/MainActivity.java
@@ -0,0 +1,14 @@
+package ngopi.malam.say;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
